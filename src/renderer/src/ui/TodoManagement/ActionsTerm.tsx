c15ee66161924a9dde48027536c1fc841324e4e0
Lok Lok <yellowhappy831@gmail.com>
2026-03-10 10:39:30 +0800

    action

--- src/renderer/src/ui/TodoManagement/ActionsTerm.tsx
+++ src/renderer/src/ui/TodoManagement/ActionsTerm.tsx
@@ -11,8 +11,10 @@ export function ActionsTerm() {
         {terminalCalls.map((eachAction, idx) => {
           return (
             <div className="p-2 m-2 border" key={`${eachAction.cmd}${idx}`}>
-              <div className="p-1 m-1 border whitespace-pre-wrap">{`${eachAction.cmd}`.trim()}</div>
-              <div className="p-1 m-1 border whitespace-pre-wrap">
+              <div className="p-2 bg-gray-100 m-2 border whitespace-pre-wrap">
+                {`${eachAction.cmd}`.trim()}
+              </div>
+              <div className="p-2 bg-gray-100 m-2 border whitespace-pre-wrap">
                 {`${eachAction.result}`.trim()}
               </div>
             </div>
