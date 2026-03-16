334b629ad6ed9e434224efdb873c4d52d5ca5512
Lok Lok <yellowhappy831@gmail.com>
2026-03-16 11:46:40 +0800

    save

--- src/renderer/src/ui/ArrayBlock/useArchApp.tsx
+++ src/renderer/src/ui/ArrayBlock/useArchApp.tsx
@@ -8,31 +8,23 @@ export const useArchApp = create(() => {
     todo: [] as any[],
 
     appName: 'works',
-    //     appUserPrompt: `
-    // I want to build an inspiration tool.
+    appUserPrompt: `
+    I want to build an inspiration tool.
 
-    // For home page "/":
-    // - I can view featured inspirations
+    For home page "/":
+      - I can view featured inspirations
 
-    // For app page "/app":
-    // - I can enter a website URL to the inspiration entry box to "save inspiration".
-    //     - When I click "save inspiration" button, we spin up a browser to take a fullpage screenshot, make a thumbnail and collect some essential text from the webpage. and then it will send to AI to generate inspirational notes. I want to use lmstudio and "uses qwen/qwen3.5-9b" AI model
-
-    // For each inspiration post page "/inspire/[id]":
-    // i can view inspiration items in the grid below the entry box.
-    // - There's a thumbnail header
-    // - Website name
-    // - Textual Analysis
-    // `.trim(),
-
-    appUserPrompt: `i want to build a todo app.`.trim(),
+    For app page "/inspire":
+      - I can enter a website URL to the inspiration entry box to "save inspiration".
+      - When I click "save inspiration" button, we spin up a browser to take a fullpage screenshot, make a thumbnail and collect some essential text from the webpage. and then it will send to AI to generate inspirational notes. I want to use lmstudio and "uses qwen/qwen3.5-9b" AI model
+  
+    For each inspiration post page "/inspire/[id]":
+    i can view inspiration items in the grid below the entry box.
+    - There's a thumbnail header
+    - Website name
+    - Textual Analysis
+    `.trim(),
 
     stream: ''
   }
 })
-
-/*
-
-You are an AI senior engineering manager agent.
-
-*/
