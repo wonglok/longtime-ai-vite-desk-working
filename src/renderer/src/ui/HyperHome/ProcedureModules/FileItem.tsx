34f70601f664285846ab9aeaea503fb745347e1b
Lok Lok <yellowhappy831@gmail.com>
2026-04-03 23:36:20 +0800

    save

--- src/renderer/src/ui/HyperHome/ProcedureModules/FileItem.tsx
+++ src/renderer/src/ui/HyperHome/ProcedureModules/FileItem.tsx
@@ -1,22 +1,27 @@
-import { Merged } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
+import { Merged, useGLTF } from '@react-three/drei'
+import { useFrame, useGraph } from '@react-three/fiber'
 import { useMemo, useRef } from 'react'
 import { Color, DoubleSide, Mesh } from 'three'
-import { RoundedBoxGeometry } from 'three/examples/jsm/Addons.js'
 import { MeshPhysicalNodeMaterial } from 'three/webgpu'
-
+import folder from '../assets/smart-folder.glb?url'
+import { RoundedBoxGeometry } from 'three/examples/jsm/Addons.js'
 export function FileItem() {
   let box = useMemo(() => {
-    let geo = new RoundedBoxGeometry(1, 1.3, 0.05, 7, 0.15 / 2)
+    let geo = new RoundedBoxGeometry(1, 1.5, 0.5, 7, 0.5)
+
+    geo.translate(0, 2, 0)
+    geo.scale(1, 1, 0.1)
+
     let mat = new MeshPhysicalNodeMaterial({
       //
       roughness: 0,
       metalness: 0,
-      color: '#78660e',
+      color: '#ffffff',
+      vertexColors: true,
       side: DoubleSide,
-      transmission: 1,
-      emissive: new Color('#ffd500'),
-      emissiveIntensity: 0.5
+      transmission: 1
+      // emissive: new Color('#000000'),
+      // emissiveIntensity: 0.5
     })
 
     let box = new Mesh(geo, mat)
@@ -24,19 +29,55 @@ export function FileItem() {
     return box
   }, [])
 
+  const glb = useGLTF(`${folder}`)
+  const { nodes } = useGraph(glb.scene)
+
+  let mat = new MeshPhysicalNodeMaterial({
+    //
+    roughness: 0,
+    metalness: 0,
+    color: '#f7cb3c',
+    emissive: '#5f5125',
+    vertexColors: true,
+    side: DoubleSide
+    // emissive: new Color('#000000'),
+    // emissiveIntensity: 0.5
+  })
+
   return (
     <>
-      <Merged meshes={[box]}>
-        {(MyBox) => {
-          let planes = []
+      <group rotation={[0, 0, 0]}>
+        <Merged
+          meshes={{
+            //
 
-          for (let i = 0; i < 20 - 1; i++) {
-            planes.push(<EachBox MyBox={MyBox} key={'plane' + i} n={20} i={i}></EachBox>)
-          }
+            ['folder-front']: new Mesh(nodes['folder-front'].geometry, mat),
+            ['folder-back']: new Mesh(nodes['folder-back'].geometry, mat),
+            box: box
+          }}
+        >
+          {(insts) => {
+            let Back = insts['folder-back']
+            let Front = insts['folder-front']
+            let Box = insts['box']
 
-          return <>{planes}</>
-        }}
-      </Merged>
+            let planes = []
+
+            for (let i = -3; i < 3; i++) {
+              planes.push(<EachBox MyBox={Box} key={'plane' + i} n={6} i={i}></EachBox>)
+            }
+
+            return (
+              <>
+                <Front></Front>
+                <Back></Back>
+
+                {planes}
+              </>
+            )
+          }}
+        </Merged>
+      </group>
     </>
   )
 }
@@ -44,15 +85,15 @@ export function FileItem() {
 function EachBox({ i, n, MyBox }) {
   let ref = useRef<any>(null)
 
-  useFrame((_, dt) => {
-    ref.current.rotation.z += dt * (i / n)
-  })
+  // useFrame((_, dt) => {
+  //   ref.current.rotation.z += dt * (i / n)
+  // })
   return (
     <MyBox
       ref={ref}
-      rotation={[Math.PI * -0.5, 0, Math.PI * 0.01 * i]}
-      position={[0, 0.1 * i, 0]}
-      color="#ffef92"
+      rotation={[0.1, 0, Math.PI * 0.05 * i]}
+      position={[0, 0.1 * i, i * 0.05]}
+      color={new Color('#ff0000').offsetHSL(i / n, 0, 0)}
     />
   )
 }
