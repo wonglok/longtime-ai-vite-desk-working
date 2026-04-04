4542c1c5d30719fcbf0f9a8b8b47eee7dde3b4dd
Lok Lok <yellowhappy831@gmail.com>
2026-04-04 14:37:22 +0800

    save

--- src/renderer/src/ui/HyperHome/PageViewFiles/PageViewFiles.tsx
+++ src/renderer/src/ui/HyperHome/PageViewFiles/PageViewFiles.tsx
@@ -2,15 +2,18 @@ import { OrbitControls } from '@react-three/drei'
 import { DeskMesh } from '../PageSelectFolder/DeskMesh'
 import { DiamondCompos } from '@renderer/ui/workspace/3d/DiamondTSL/DiamondComponent'
 import { Spinner } from '@renderer/ui/workspace/3d/DiamondTSL/DiamondCanvas'
+import { useHome } from '../useHome'
 
 export function PageViewFiles({ workspace }: { workspace: string }) {
+  console.log('workspace', workspace)
+
   return (
     <>
       <OrbitControls
         makeDefault
         object-position={[0, 2, 3.5]}
         target={[0, 1, 0]}
-        minDistance={3}
+        minDistance={1.5}
         maxDistance={7}
         minAzimuthAngle={-0.25 * Math.PI}
         maxAzimuthAngle={0.25 * Math.PI}
@@ -20,12 +23,22 @@ export function PageViewFiles({ workspace }: { workspace: string }) {
 
       <DeskMesh></DeskMesh>
 
-      <group position={[0, 1, 0]}>
+      <group
+        //
+        position={[0, 1, 0]}
+        //
+        onClick={() => {
+          useHome.setState({
+            pageAt: 'home'
+          })
+        }}
+      >
         <Spinner>
           <DiamondCompos></DiamondCompos>
         </Spinner>
       </group>
       {/*  */}
+
       {/*  */}
     </>
   )
